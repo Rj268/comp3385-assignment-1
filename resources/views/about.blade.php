8129944227416d30e2c8a78a73a8c137bef00106
Ronnie Samuel <samuelronnie55@gmail.com>
2025-02-10 15:36:06 -0400

    Updated About page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends ('layouts.app')
+
+@section('content')
+    <h2>Ronnie Samuel</h2>
+    <p>I am a Computer Science student with an interest in Web Development</p>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/about', function () {
-    return "My About Page";
-});
+Route::get('/about', [AboutController::class, 'show']);
 
