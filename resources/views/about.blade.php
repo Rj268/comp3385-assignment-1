f578383cae4c11ba56b2fef4b8c07eb66359ddf3
Ronnie Samuel <samuelronnie55@gmail.com>
2025-02-10 16:08:26 -0400

    Added CSS styling and image to About page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,7 @@
 @extends ('layouts.app')
 
 @section('content')
+    <img src="{{url('/images/RJ.webp')}}" alt="Image of Ronnie Samuel">
     <h2>Ronnie Samuel</h2>
     <p>I am a Computer Science student with an interest in Web Development</p>
 
