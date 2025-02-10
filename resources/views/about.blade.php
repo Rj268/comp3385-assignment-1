9c466fa7365306d97a588c92ac6f352808523eac
Ronnie Samuel <samuelronnie55@gmail.com>
2025-02-10 16:15:31 -0400

    Updated About page and styled image

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,9 @@
 @section('content')
     <img src="{{url('/images/RJ.webp')}}" alt="Image of Ronnie Samuel">
     <h2>Ronnie Samuel</h2>
-    <p>I am a Computer Science student with an interest in Web Development</p>
+    <p>I am a Computer Science student with a passion for Web Development and Software Engineering. <br>
+        I enjoy learning new technologies, building applications, and solving real-world problems through coding. <br>
+        My interests also include cybersecurity, system design, and database management.
+    </p>
 
 @endsection
